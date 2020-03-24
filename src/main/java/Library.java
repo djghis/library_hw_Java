7932ca9d5b83227f53217037eb6a813506bef11c
djghis <djghis@hotmail.com>
2020-03-24 21:09:49 +0000

    library set up and start empty test passing

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> bookCollection;
+
+    public Library(){
+        this.bookCollection = new ArrayList<Book>();
+    }
+
+
+    public int bookCount() {
+        return bookCollection.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+    private Library library;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+    private Book book4;
+
+    @Before
+    public void setUp(){
+        library = new Library();
+        book1 = new Book("Da Vinci Code", "Dan Brown", "Thriller");
+        book2 = new Book("Harry Potter 1", "JK Rowling", "Fantasy");
+        book3 = new Book("Harry Potter 2", "JK Rowling", "Fantasy");
+        book4 = new Book("Harry Potter 3", "JK Rowling", "Fantasy");
+    }
+
+    @Test
+    public void libraryStartEmpty(){
+        assertEquals(0, library.bookCount());
+    }
 }
